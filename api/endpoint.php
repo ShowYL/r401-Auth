4131689d9c18a28cf09be023a75668c4e0e9a247
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-03-06 09:22:58 +0100

    Implement JWT authentication with GET to check the token

--- api/endpoint.php
+++ api/endpoint.php
@@ -2,23 +2,25 @@
 require_once '../model/gestionAuth.php';
 require_once '../model/utils.php';
 
-switch($_SERVER["REQUEST_METHOD"]){
+const timeExpire = 60 ; # time in second for the JWT token to expire currently 1 minute
+
+switch ($_SERVER["REQUEST_METHOD"]) {
 
     case "POST":
         $postedData = file_get_contents("php://input");
         $data = json_decode($postedData, true);
-        if(isset($data["username"]) && isset($data["password"])){
+        if (isset($data["username"]) && isset($data["password"])) {
             $username = $data["username"];
             $password = $data["password"];
-            if(getUser($username, $password)) {
+            if (getUser($username, $password)) {
                 $headers = array('alg' => 'HS256', 'typ' => 'JWT');
-                $payload = array('login' => $username, 'role' => getRole($username), 'exp' => time() + 60);
+                $payload = array('login' => $username, 'role' => getRole($username), 'exp' => time() + timeExpire);
                 $jwt = generate_jwt($headers, $payload);
-                deliverResponse(200, "User identified successfully",data:$jwt);
-            }else{
+                deliverResponse(200, "User identified successfully", data: $jwt);
+            } else {
                 deliverResponse(400, "Error wrong credential");
             }
-        }else{
+        } else {
             deliverResponse(400, "Invalid request");
         }
         break;
@@ -26,16 +28,24 @@ switch($_SERVER["REQUEST_METHOD"]){
     case "PUT":
         $postedData = file_get_contents("php://input");
         $data = json_decode($postedData, true);
-        if(isset($data["username"]) && isset($data["password"])){
+        if (isset($data["username"]) && isset($data["password"])) {
             $username = $data["username"];
             $password = $data["password"];
-            if(createUser($username, $password)) {
+            if (createUser($username, $password)) {
                 deliverResponse(201, "User created successfully");
-            }else{
+            } else {
                 deliverResponse(400, "Error while creating the user");
             }
-        }else{
+        } else {
             deliverResponse(400, "Invalid request");
+        }
+        break;
+
+    case "GET":
+        if (get_bearer_token() && is_jwt_valid(get_bearer_token())) {
+            deliverResponse(200, "Token is valid");
+        } else {
+            deliverResponse(400, "Invalid Token");
         }
         break;
 
