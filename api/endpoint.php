b2664467b52fb5fe270528432c1bfc03115462f8
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-03-13 08:12:59 +0100

    endpoint modif to correct CROS

--- api/endpoint.php
+++ api/endpoint.php
@@ -4,6 +4,11 @@ require_once '../model/utils.php';
 
 const timeExpire = 60 ; # time in second for the JWT token to expire currently 1 minute
 
+header('Content-Type:application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
 switch ($_SERVER["REQUEST_METHOD"]) {
 
     case "POST":
